0e0669dd02a8f2d79aadb1706340e0a7cbc72e6b
Marcelo Sousa <vinicusm466@gmail.com>
2026-02-25 23:10:12 -0300

    Terminei o primeiro exercício

--- Ex01/Program.cs
+++ Ex01/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿// Números pares entre 1 e número aleatório sorteado
+
+Random numero = new Random();
+
+int valor = numero.Next(1,101);
+
+for(int i = 1; i <= valor; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
